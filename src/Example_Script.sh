29b427277f9bbd619650ca1613ba721afabf983e
Sylhare <sylhareng@gmail.com>
2017-03-09 16:30:16 -0500

    initial Commit

---
+++ src/Example_Script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash                #shbang line Each Scripts should specify at the beginning, 
+                           # in which shell it should be run. here is bash
+
+echo "Running"             #will print Running on the command line
+clear                      #clear the command screen
+echo "text" >> file.txt    #append text to a file
+date >> file.txt           #return the date and time
